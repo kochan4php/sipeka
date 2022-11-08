9835ef0260b91e2603a4e13c4063fa16218eff52
kochan4php <aprodeosubarno@gmail.com>
2022-11-08 21:06:50 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+  <title>Beranda | SIPEKA</title>
+</head>
+
+<body>
+  <div class="container my-4">
+    <h1 class="text-center">Selamat datang di aplikasi SIPEKA</h1>
+  </div>
+</body>
+
+</html>
