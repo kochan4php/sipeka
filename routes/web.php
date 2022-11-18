2b87c4b73acf8136acd9760fa88ac0dd10b764af
kochan4php <aprodeosubarno@gmail.com>
2022-11-18 09:25:17 +0700

    membuat halaman list pelamar

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,9 @@ Route::prefix('/sipeka')->group(function () {
 
   Route::prefix('/admin')->group(function () {
     Route::get('/', fn () => view('admin.index'))->name('admin.index');
+    Route::get('/alumni', fn () => view('admin.alumni.index'))->name('admin.alumni.index');
+    Route::get('/alumni/tambah', fn () => view('admin.alumni.tambah'))->name('admin.alumni.tambah');
+    Route::get('/alumni/detail/{kode_alumni}', fn ($kode_alumni) => "{$kode_alumni}")->name('admin.alumni.detail');
   });
 
   Route::get('/pelamar', fn () => 'Halo ini halaman pelamar');
