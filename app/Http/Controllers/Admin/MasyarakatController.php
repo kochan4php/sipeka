acbdb59c03782ef6658c3001bf115d5dcaf5c7b5
kochan4php <aprodeosubarno@gmail.com>
2022-11-19 16:32:03 +0700

    membuat halaman dan controller untuk masyarakat (pelamar dari luar sekolah

---
+++ app/Http/Controllers/Admin/MasyarakatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MasyarakatController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return view('admin.masyarakat.index');
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('admin.masyarakat.tambah');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    return 'Hehe berhasil';
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    return view('admin.masyarakat.detail', compact('id'));
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    return view('admin.masyarakat.sunting');
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    return 'Hehe berhasil';
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    return 'Berhasil hapus data';
+  }
+}
---
+++ resources/views/admin/masyarakat/sunting.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.dashboard.app')
+
+@section('container-dashboard')
+  <div class="row pt-3 pb-1 mb-1">
+    <div class="col">
+      <div class="card">
+        <div class="card-header pb-0">
+          <h2>Sunting data pelamar</h2>
+        </div>
+        <div class="card-body">
+          <form action="{{ route('admin.pelamar.update', 'layla-mayrisa') }}" method="POST">
+            @csrf
+            @method('put')
+            <div class="mb-3 row">
+              <label for="nama" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('Nama Lengkap') }}
+              </label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="inputPassword" name="nama"
+                  placeholder="Aphrodeo Subarno">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="jenis-kelamin" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('Jenis Kelamin') }}
+              </label>
+              <div class="col-sm-8">
+                <select name="jenis-kelamin" id="jenis-kelamin" class="form-select">
+                  <option selected>-- Pilih jenis kelamin --</option>
+                  <option value="L">Laki-laki</option>
+                  <option value="P">Perempuan</option>
+                </select>
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="tempat-lahir" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('Tempat Lahir') }}
+              </label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="tempat-lahir" name="tempat-lahir" placeholder="Bekasi">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="tanggal-lahir" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('Tanggal Lahir') }}
+              </label>
+              <div class="col-sm-8">
+                <input type="date" class="form-control" id="tanggal-lahir" name="tanggal-lahir"
+                  placeholder="04/18/2005">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="no-telepon" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('No. Telepon') }}
+              </label>
+              <div class="col-sm-8">
+                <input type="number" class="form-control" id="no-telepon" placeholder="08988976056">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="alamat-pelamar" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('Alamat Tempat Tinggal') }}
+              </label>
+              <div class="col-sm-8">
+                <textarea class="form-control" placeholder="Leave a comment here" id="alamat-pelamar" name="alamat-pelamar"
+                  rows="4"></textarea>
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="foto" class="col-sm-4 col-form-label text-md-end fs-6 fs-md-5">
+                {{ __('Foto') }}
+              </label>
+              <div class="col-sm-8">
+                <input type="file" class="form-control" id="foto">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-sm-4"></div>
+              <div class="col-sm-8 d-flex gap-2">
+                <button type="submit" class="btn btn-primary">Perbarui</button>
+                <a href="{{ route('admin.pelamar.index') }}" class="btn btn-danger">Batal</a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
