cec47eb77f6006470cca75defb7c4b217a458786
kochan4php <aprodeosubarno@gmail.com>
2022-11-22 07:24:22 +0700

    menampilkan list masyarakat dan detail masyarakat dari database ke view blade, serta perbaikan kecil

--- app/Http/Controllers/Admin/MasyarakatController.php
+++ app/Http/Controllers/Admin/MasyarakatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MasyarakatController extends Controller
 {
@@ -14,7 +15,20 @@ class MasyarakatController extends Controller
    */
   public function index()
   {
-    return view('admin.pengguna.masyarakat.index');
+    $masyarakat = collect(DB::select(
+      "SELECT
+        m.id_masyarakat,
+        p.id_pelamar,
+        u.id_user,
+        m.nama_lengkap,
+        m.tanggal_lahir,
+        m.no_telepon,
+        u.username
+      FROM masyarakat AS m
+      INNER JOIN pelamar AS p ON m.id_pelamar = p.id_pelamar
+      INNER JOIN users AS u ON p.id_user = u.id_user"
+    ));
+    return view('admin.pengguna.masyarakat.index', compact('masyarakat'));
   }
 
   /**
@@ -44,9 +58,32 @@ class MasyarakatController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function show($id)
+  public function show($username)
   {
-    return view('admin.pengguna.masyarakat.detail', compact('id'));
+    $masyarakat = collect(DB::select(
+      "SELECT
+        m.id_masyarakat,
+        p.id_pelamar,
+        u.id_user,
+        lu.id_level,
+        m.nama_lengkap,
+        m.jenis_kelamin,
+        m.tempat_lahir,
+        m.tanggal_lahir,
+        m.alamat_tempat_tinggal,
+        m.no_telepon,
+        m.foto,
+        u.username,
+        u.email,
+        lu.nama_level
+      FROM masyarakat AS m
+      INNER JOIN pelamar AS p ON m.id_pelamar = p.id_pelamar
+      INNER JOIN users AS u ON p.id_user = u.id_user
+      INNER JOIN level_user AS lu ON u.id_level = lu.id_level
+      WHERE u.username = ?",
+      [$username]
+    ))->first();
+    return view('admin.pengguna.masyarakat.detail', compact('masyarakat'));
   }
 
   /**
