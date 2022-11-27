f2ae82d53b3cf613774a0641dc3b172003c64595
kochan4php <aprodeosubarno@gmail.com>
2022-11-28 04:50:37 +0700

    memperbaiki charset dan collation pada masing masing tabel

--- database/migrations/2022_11_08_205830_create_level_user_table.php
+++ database/migrations/2022_11_08_205830_create_level_user_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
   {
     Schema::create('level_user', function (Blueprint $table) {
       $table->engine = env('DB_STORAGE_ENGINE', 'InnoDB');
-      $table->charset = env('DB_CHARSET', 'utf8');
-      $table->collation = env('DB_COLLATION', 'utf8_unicode_ci');
+      $table->charset = env('DB_CHARSET', 'utf8mb4');
+      $table->collation = env('DB_COLLATION', 'utf8mb4_general_ci');
       $table->char('id_level', 4)->primary();
       $table->string('nama_level', 20);
     });
