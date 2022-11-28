185b07d78a942d689985f7dd8aa05062afabf380
kochan4php <aprodeosubarno@gmail.com>
2022-11-28 21:51:27 +0700

    menambahkan dan menggunakan owl carousel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
 
   @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/owl-carousel/dist/assets/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/owl-carousel/dist/assets/owl.theme.default.min.css') }}">
 
   <title>Beranda | SIPEKA</title>
 </head>
@@ -15,24 +17,28 @@
 <body>
   @include('layouts.navigation')
 
-  <div id="carouselExampleControls" class="carousel slide" style="min-height: 80vh !important;" data-bs-ride="carousel">
+  <div id="carouselExampleControls" class="carousel slide" style="min-height: 1000px !important;"
+    data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="://picsum.photos/id/11/2500/2500" class="d-block w-100" alt="...">
+        <img src="{{ asset('assets/images/5.jpg') }}" style="height: 1000px; object-fit: cover; object-position: center"
+          class="img-fluid d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h1>First slide label</h1>
           <p>Some representative placeholder content for the first slide.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://picsum.photos/id/237/5000/5000" class="d-block w-100" alt="...">
+        <img src="{{ asset('assets/images/5.jpg') }}" style="height: 1000px; object-fit: cover; object-position: center"
+          class="img-fluid d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h1>Second slide label</h1>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://picsum.photos/id/1/2500/2500" class="d-block w-100" alt="...">
+        <img src="{{ asset('assets/images/5.jpg') }}" style="height: 1000px; object-fit: cover; object-position: center"
+          class="img-fluid d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
           <h1>Third slide label</h1>
           <p>Some representative placeholder content for the third slide.</p>
@@ -52,6 +58,10 @@
   <div class="container my-4">
     @yield('container')
   </div>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+  <script src="{{ asset('assets/owl-carousel/dist/owl.carousel.min.js') }}"></script>
+
+  @stack('script-owl')
 </body>
 
 </html>
