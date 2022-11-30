5d7f3c58ab139a067f392d776c88f3527e26b385
kochan4php <aprodeosubarno@gmail.com>
2022-11-30 07:46:32 +0700

    membuat view agregate dan menampilkannya di browser

---
+++ app/Http/Controllers/Admin/MainController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class MainController extends Controller
+{
+  public function __invoke()
+  {
+    $jumlah_pengguna = collect(DB::select("SELECT * FROM jumlah_pengguna"))
+      ->firstOrFail()
+      ->jumlah_pengguna;
+
+    $jumlah_alumni = collect(DB::select("SELECT * FROM jumlah_alumni"))
+      ->firstOrFail()
+      ->jumlah_alumni;
+
+    $jumlah_masyarakat = collect(DB::select("SELECT * FROM jumlah_masyarakat"))
+      ->firstOrFail()
+      ->jumlah_masyarakat;
+
+    $jumlah_mitra_perusahaan = collect(DB::select("SELECT * FROM jumlah_mitra_perusahaan"))
+      ->firstOrFail()
+      ->jumlah_mitra_perusahaan;
+
+    $jumlah_jurusan = collect(DB::select("SELECT * FROM jumlah_jurusan"))
+      ->firstOrFail()
+      ->jumlah_jurusan;
+
+    $jumlah_angkatan = collect(DB::select("SELECT * FROM jumlah_angkatan"))
+      ->firstOrFail()
+      ->jumlah_angkatan;
+
+    return view('admin.index', compact(
+      'jumlah_pengguna',
+      'jumlah_alumni',
+      'jumlah_masyarakat',
+      'jumlah_mitra_perusahaan',
+      'jumlah_jurusan',
+      'jumlah_angkatan'
+    ));
+  }
+}
