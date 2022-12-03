ef4c2146b78fad1eab34c3fda889f0d2023ae713
kochan4php <aprodeosubarno@gmail.com>
2022-12-04 00:42:30 +0700

    membuat controller action untuk login

--- resources/views/layouts/dashboard/header.blade.php
+++ resources/views/layouts/dashboard/header.blade.php
@@ -2,9 +2,9 @@
   class="navbar navbar-dark sticky-top bg-white bg-md-primary border-bottom border-2 border-secondary flex-md-nowrap p-0 d-flex justify-content-between align-items-center shadow-sm"
   style="--bs-border-opacity: .2;">
   <a class="navbar-brand bg-primary col-md-3 col-lg-2 me-0 px-3 fs-5 fw-bold leading-1px lead"
-    href="{{ route('admin.index') }}">SIPEKA Admin</a>
+    href="{{ route('admin.index') }}">{{ Auth::user()->level_user->nama_level }}</a>
   <a class="navbar-brand bg-transparent text-black col-md-3 d-none d-md-inline-block col-lg-2 me-0 px-3 fs-5 text-left fw-bold leading-1px lead"
-    href="{{ route('home') }}">SIPEKA</a>
+    href="{{ route('home') }}">SIPEKA {{ Auth::user()->level_user->nama_level }}</a>
   <button class="navbar-toggler position-absolute collapsed d-md-none" type="button" data-bs-toggle="collapse"
     data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
