4a1d3475f1358dc9d713d03f1caf6497040f136f
kochan4php <aprodeosubarno@gmail.com>
2022-12-03 16:30:57 +0700

    membuat halaman experience dan tambah experience, serta perbaikan kecil lainnya

---
+++ resources/views/pelamar/action.blade.php
@@ -0,0 +1,22 @@
+<div class="col-lg-4">
+  <img src="{{ asset('assets/images/6.jpeg') }}" style="height: 290px; object-fit: cover; object-position: center"
+    class="img-fluid d-block w-100 rounded" alt="...">
+  <div class="btn-group-vertical w-100 mt-4">
+    <a href="{{ route('pelamar.profile', 'cina') }}"
+      class="btn @if (Request::is('sipeka/pelamar/profile*')) bg-dark text-white @endif btn-outline-dark btn-block fs-5 btn-sm">
+      Profil saya
+    </a>
+    <a href="{{ route('pelamar.dokumen', 'cina') }}"
+      class="btn @if (Request::is('sipeka/pelamar/dokumen*')) bg-dark text-white @endif btn-outline-dark btn-block fs-5 btn-sm">
+      Dokumen saya
+    </a>
+    <a href="{{ route('pelamar.experience.index', 'cina') }}"
+      class="btn @if (Request::is('sipeka/pelamar/pengalaman-kerja*')) bg-dark text-white @endif btn-outline-dark btn-block fs-5 btn-sm">
+      Pengalaman Kerja
+    </a>
+    <a href="{{ route('pelamar.profile', 'cina') }}"
+      class="btn @if (Request::is('sipeka/pelamar/progress/cina*')) bg-dark text-white @endif btn-outline-dark btn-block fs-5 btn-sm">
+      Progress lamaran kerja saya
+    </a>
+  </div>
+</div>
