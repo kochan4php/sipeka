bf7ee75534663969018d64acc8a19f19ea74fe53
kochan4php <aprodeosubarno@gmail.com>
2022-12-08 10:14:25 +0700

    perbaikan pada procedure untuk update data alumni

--- database/migrations/2022_11_27_144725_create_procedure_update_one_siswa_alumni_by_username.php
+++ database/migrations/2022_11_27_144725_create_procedure_update_one_siswa_alumni_by_username.php
@@ -12,9 +12,9 @@ return new class extends Migration
    */
   public function up()
   {
-    DB::unprepared("DROP PROCEDURE IF EXISTS update_one_siswa_alumni_by_nis");
+    DB::unprepared("DROP PROCEDURE IF EXISTS update_one_siswa_alumni_by_username");
     DB::unprepared(
-      "CREATE PROCEDURE update_one_siswa_alumni_by_nis(old_username varchar(255), new_username varchar(255), password varchar(255), jurusan char(7), tahun_angkatan char(8), nis varchar(18), nama_lengkap varchar(255), jenis_kelamin enum('L', 'P'), tempat_lahir varchar(100), tanggal_lahir date, no_telepon varchar(20), alamat_tempat_tinggal text, foto varchar(255))
+      "CREATE PROCEDURE update_one_siswa_alumni_by_username(old_username varchar(255), new_username varchar(255), password varchar(255), jurusan char(7), tahun_angkatan char(8), nis varchar(18), nama_lengkap varchar(255), jenis_kelamin enum('L', 'P'), tempat_lahir varchar(100), tanggal_lahir date, no_telepon varchar(20), alamat_tempat_tinggal text, foto varchar(255))
       BEGIN
         DECLARE id_user int;
         DECLARE old_nis varchar(18);
@@ -33,6 +33,13 @@ return new class extends Migration
             SET siswa_alumni.nis = nis WHERE users.id_user = id_user;
         END IF;
 
+        IF (foto IS NOT NULL) THEN
+          UPDATE siswa_alumni
+            INNER JOIN pelamar ON siswa_alumni.id_pelamar = pelamar.id_pelamar
+            INNER JOIN users ON pelamar.id_user = users.id_user
+            SET siswa_alumni.foto = foto WHERE users.id_user = id_user;
+        END IF;
+
         UPDATE siswa_alumni
           INNER JOIN pelamar ON siswa_alumni.id_pelamar = pelamar.id_pelamar
           INNER JOIN users ON pelamar.id_user = users.id_user
@@ -44,8 +51,7 @@ return new class extends Migration
             siswa_alumni.tempat_lahir = tempat_lahir,
             siswa_alumni.tanggal_lahir = tanggal_lahir,
             siswa_alumni.no_telepon = no_telepon,
-            siswa_alumni.alamat_tempat_tinggal = alamat_tempat_tinggal,
-            siswa_alumni.foto = foto
+            siswa_alumni.alamat_tempat_tinggal = alamat_tempat_tinggal
           WHERE users.id_user = id_user;
       END ;"
     );
