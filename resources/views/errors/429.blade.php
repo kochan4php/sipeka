f9badcde85a9c8a795600aec869064b7f25c3ce1
kochan4php <aprodeosubarno@gmail.com>
2023-01-13 22:36:34 +0700

    membuat halaman error

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,15 @@
+@extends('errors.layouts', ['title' => '403 Forbidden'])
+
+@section('error')
+  <div class="row justify-content-center align-items-center">
+    <div class="col-md-6">
+      <div class="card text-center">
+        <div class="card-body">
+          <h1 class="not-found-title">429</h1>
+          <p class="fs-5 mb-3"><span class="text-danger">Opps!</span> {{ $exception->getMessage() ?? 'Forbidden' }}</p>
+          <a href="{{ route('home') }}" class="btn btn-primary">Kembali</a>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
